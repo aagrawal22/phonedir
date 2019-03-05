7194283a23d0e6e286e4800c62019d1f7a008567
Abhishek Agrawal <abhishek18agrawal@gmail.com>
2019-03-05 13:36:34 +0530

    Updates

---
+++ precache-manifest.2c5a1f6439d1352aabdc6d56a82de99a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4123233a352bd8dd4b09",
+    "url": "/phonedir/static/js/runtime~main.4123233a.js"
+  },
+  {
+    "revision": "868e3562438b2c8fc05f",
+    "url": "/phonedir/static/js/main.868e3562.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/phonedir/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "5e33e06adeecf5e8d16480c78f232e61",
+    "url": "/phonedir/index.html"
+  }
+];
